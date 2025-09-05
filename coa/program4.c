107745c27c4494918fcc5ede11d88bb068c7b474
unknown <129291511+dedsocks@users.noreply.github.com>
2025-09-05 15:17:46 +0530

    Add 1's , 2's complement functions

--- coa/program4.c
+++ coa/program4.c
@@ -10,8 +10,10 @@ void inputBinaryNumber(int* binaryNumber, int serialNumber){
     }
 }
 
-void addBinaryNumbers(int* binaryNumber1, int* binaryNumber2, int* finalNumber){
+void addBinaryNumbers(int* binaryNumber1, int* binaryNumber2){
     int i, carry = 0;
+    int finalNumber[NUMBER_OF_BITS] = binaryNumber1;
+
     for(i = NUMBER_OF_BITS-1; i >= 0; i--){
         finalNumber[i] = binaryNumber1[i] + binaryNumber2[i] + carry;
         if(finalNumber[i] >= 2){
@@ -20,6 +22,28 @@ void addBinaryNumbers(int* binaryNumber1, int* binaryNumber2, int* finalNumber){
         else carry = 0;
         finalNumber[i] %= 2;
     }
+}
+
+void find1sComplement(int* binaryNumber){
+    for(int i = 0; i < NUMBER_OF_BITS; i++){
+        if(binaryNumber[i] == 0){
+            binaryNumber[i] = 1;
+        }
+        else binaryNumber[i] = 0;
+    }
+}
+
+void find2sComplement(int* binaryNumber){
+    int addToLsb[NUMBER_OF_BITS] = {0};
+    addToLsb[NUMBER_OF_BITS - 1] = 1;
+
+    find1sComplement(binaryNumber);
+
+    addBinaryNumbers(binaryNumber, addToLsb);
+}
+
+void subtractBinaryNumbers(int* minuend, int* subtraend){
+
 }
 
 void displayBinaryNumber(int* binaryNumber, int numberOfBits){
